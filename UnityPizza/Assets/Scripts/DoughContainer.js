31789d814d5ee44375062cb022d45a7afcdc2af6
Matias Altalef <maltalef@gmail.com>
2015-01-24 11:11:37 -0300

    prototipo a medio hacer y placeholder art

---
+++ UnityPizza/Assets/Scripts/DoughContainer.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Awake () {
+}
+
+function Start () {
+	
+}
+
+function Update () {
+
+}
+
+function OnMouseDown() {
+	var theGame = Game.Instance();
+	theGame.MakeSomePizza();
+}
---
+++ UnityPizza/Assets/Scripts/Game.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+static var theInstance : Game;
+
+var pizzaPlace: Transform; // ACA VA LA PIZZA (SOBRE LA MESADA)
+
+var initialDough: GameObject;
+
+static function Instance () : Game {
+	return theInstance;
+}
+
+function Start () {
+	theInstance = this;
+	
+	pizzaPlace = GameObject.Find("pizzaPlace").GetComponent(Transform);
+	
+	
+//	Debug.Log(initialDough);
+	
+	initialDough.renderer.enabled = false;
+}
+
+function Update () {
+
+}
+
+function MakeSomePizza () {
+//	pizzaPlace.position;
+	initialDough.renderer.enabled = true;
+}
