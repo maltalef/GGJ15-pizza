48c00071622606d0fac2c50d7ea9f0a307be9e99
Matias Altalef <maltalef@gmail.com>
2015-01-27 11:01:58 -0300

    ahora se adapta a varias resoluciones pero los sprites se resizean medio feo... too bad, aprendizaje para el proximo juego

---
+++ UnityPizza/Assets/Scripts/CustomCamera.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+// http://gamedev.stackexchange.com/a/89973/12103
+
+// background is 1122 x 631 px
+// in world units, it should be 11.22 x 6.31
+// alas, it's something else.
+// whatever :)
+
+var background: SpriteRenderer;
+
+function Start () {
+	
+	var width = background.bounds.extents.x * 2;
+	var height = background.bounds.extents.y * 2;
+	
+	var ortho = width / camera.aspect / 2f;
+	if (ortho < height / 2f)
+		ortho = height / 2f;
+	camera.orthographicSize = ortho;//1f / camera.aspect * width / 2f;
+}
